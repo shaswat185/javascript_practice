5112a15ff762fe4aa349c36b29b43af4dee32bf0
Shaswat Dubey <shaswat185dubey@gmail.com>
2024-06-22 15:31:30 +0530

    new

--- filter.js
+++ filter.js
@@ -1,7 +1,16 @@
-const data1=[1,2,3,4,5,6,7,8,9];
+// const data1=[1,2,3,4,5,6,7,8,9];
 
-const data2=data1.filter((e)=>{
-    return e%2==0;
+// const data2=data1.filter((e)=>{
+//     return e%2==0;
+// })
+
+// console.log(data2)
+
+
+
+const new_data=[2,3,4,5,6,7,8,9]
+let data=new_data.filter((e)=>{
+    return e<5;
 })
 
-console.log(data2)
+console.log(data)
--- reduce.js
+++ reduce.js
@@ -1,6 +1,15 @@
-const data1=[1,2,3,4,5,6,7,8,9];
-const data2=data1.reduce((prev,curr)=>{
-    return prev+curr;
+// const data1=[1,2,3,4,5,6,7,8,9];
+// const data2=data1.reduce((prev,curr)=>{
+//     return prev+curr;
+// })
+
+// console.log(data2)
+
+
+
+let a=[2,3,4,5,6,7,8,9];
+let b= a.reduce((e1,e2)=>{
+    return e1+e2;
 })
 
-console.log(data2)
+console.log(b);
